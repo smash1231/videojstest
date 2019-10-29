8035478d552542be11d22b00fdc7bf2e395300a0
smash1231 <smashash1231@gmail.com>
2019-10-29 22:26:37 +0100

    add meta data to startvideo

--- startvideo.js
+++ startvideo.js
@@ -5,8 +5,13 @@ function init() {        // Master function, encapsulates all functions
       getVideo();
     function getVideo(){
          var fileURL = GetUrlValue('video-m3u8');  //document.getElementById("videoFile").value;  // get input field          
-        var thumbURL = GetUrlValue('thumb');
+        var thumbURL = decodeURIComponent( GetUrlValue('thumb') );
         var testURL = 'https://cs9-13v4.vkuservideo.net/video/hls/p8/979ce8d947f3/index-f5-v1-a1.m3u8?extra=Xh0kwewdp_9XU9Xbss8xRwuwUvurZkRZA1QsGy9vJwXraZuYp_Qwp5d6LYIeZy0KwmzvMQ_zbAoglyrbkSGZf5J4mCyzfrHM4KLSpCDS-g57KXGqVI3dnckrGSo3j6r8zzZQK6FXhSIJBsMdjVxSJIrtrpw'
+        var meta = document.createElement('meta');
+        meta.property= "og:image";
+        meta.content = thumbURL ;
+        document.getElementsByTagName('head')[0].appendChild(meta);
+        
         if (fileURL != "") {
             type = 'video/mp4'
             if(fileURL.indexOf('m3u8') != -1)
@@ -20,7 +25,7 @@ function init() {        // Master function, encapsulates all functions
             console.log("type " + type);
              player = window.player = videojs('my-video0');
 
-            player.poster(decodeURIComponent(thumbURL));
+            player.poster(thumbURL);
              player.src( 
                     //sources: [{src:  decodeURIComponent(fileURL), type:type, poster:decodeURIComponent(thumbURL)}],
                     {src:  decodeURIComponent(fileURL), type:type});
