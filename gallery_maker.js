27df0982c7d027cd6614815e2f14d4f565441dbb
smash1231 <smashash1231@gmail.com>
2020-01-10 20:54:39 +0100

    add downlaod button to gallery

--- gallery_maker.js
+++ gallery_maker.js
@@ -1,8 +1,11 @@
+highResUrlArray = []
+
 function pad(n, width, z) {
   z = z || '0';
   n = n + '';
   return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
+
 
 function GetUrlArray(inputUrl)
 {
@@ -31,6 +34,72 @@ function GetUrlArray(inputUrl)
 
 }
 
+function copyToClipboard(text) {
+    var dummy = document.createElement("textarea");
+    // to avoid breaking orgain page when copying more words
+    // cant copy when adding below this code
+    // dummy.style.display = 'none'
+    document.body.appendChild(dummy);
+    //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
+    dummy.value = text;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+}
+
+function getFileName(url)
+{
+    return  url.replace(/^.*[\\\/]/, '');
+}
+
+
+function getZipFile()
+{
+    var zip = new JSZip();
+    var button = document.getElementById("downloadButton");
+    button.removeEventListener("click", getZipFile , false);
+    button.innerHTML = "please wait. Downloading files and creating zip file..."
+
+    var urls = highResUrlArray;
+    function request(url) {
+        return new Promise(function(resolve) {
+
+            JSZipUtils.getBinaryContent(url,function (err, data) 
+            {
+                if(!err)
+                {
+                    var dic={base64:true,binary:true}; //WAV File Encoding
+                    var filename = getFileName(url);
+                    zip.file(filename, data, dic);
+                    button.innerHTML = "Please Wait. Fetching files: " + filename;
+                    resolve();
+                 }else{
+                     console.log("error " + err.message);
+                 }
+            });
+
+        })
+      }
+
+    Promise.all(urls.map(function(url) {
+          return request(url)
+    })).then(function() {
+          console.log(zip);
+          button.innerHTML = "please wait. zipping files " 
+          zip.generateAsync({
+              compression: "DEFLATE", // <-- here
+              type: "blob", 
+          },function updateCallback(metadata) {
+            button.innerHTML = "Please Wait. Zipping files " + metadata.percent + '%';}).then(function(content) {
+                  saveAs(content, "images.zip");
+                    button.addEventListener("click", getZipFile, false);
+                    button.innerHTML = "Download images in zip"
+
+            });
+        });
+}
+
+
 function createGallery()
 {
     var thumbsUrl = decodeURIComponent( GetUrlValue('thumbs') );
@@ -43,6 +112,7 @@ function createGallery()
 
 
     var imageUrls = GetUrlArray(imagesUrl);
+    highResUrlArray = imageUrls;
     var thumbUrls = GetUrlArray(thumbsUrl);
     var imageLowUrls = imageUrls;
     if(imagesLowResUrl != undefined && imagesLowResUrl != null && imagesLowResUrl.length > 10) 
@@ -56,18 +126,21 @@ function createGallery()
 
     }
 
-         var b =document.getElementsByClassName('galleria')[0];
-      b.innerHTML = htmlString;
+    var b =document.getElementsByClassName('galleria')[0];
+    b.innerHTML = htmlString;
 
-      Galleria.loadTheme('azur/galleria.azur.min.js');
+    Galleria.loadTheme('azur/galleria.azur.min.js');
     Galleria.configure({
         imageCrop: false,
         popupLinks: true
     });
       Galleria.run('.galleria');
 
+    document.getElementById("downloadButton").addEventListener("click", getZipFile, false);
 
 }
+
+
 
 function GetUrlValue(VarSearch){
     var SearchString = window.location.search.substring(1);
@@ -85,3 +158,6 @@ function GetUrlValue(VarSearch){
 document.addEventListener('DOMContentLoaded', function () {
     createGallery();
 });  
+
+
+
